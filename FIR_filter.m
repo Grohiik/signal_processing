374e787c6839d26ee868675474c6c56145b9a49b
Heart <heartbrrrrr@protonmail.com>
2022-02-24 09:58:58 +0100

    Updated for lab 4
    Code files and filter coefficients for lab 4

--- FIR_filter.m
+++ FIR_filter.m
@@ -1,5 +1,7 @@
 fs=10000;
 f=(0:0.01:5000);
 omega=2*pi*f/fs;
-H=1/4+1/4*exp(-i*omega)+1/4*exp(-i*omega*2)+1/4*exp(-i*omega*3);
+H=(1/10)./(1-(0.9*exp(-omega*j*1)));
 plot(f,abs(H))
+
+saveas(gcf, "filer\plot.png")
--- src/main.cpp
+++ src/main.cpp
@@ -4,69 +4,79 @@ volatile int count;
 hw_timer_t *timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-#define testPin GPIO_NUM_21
-#define inputPin GPIO_NUM_32
+#define testPin   GPIO_NUM_21
+#define inputPin  GPIO_NUM_32
 #define outputPin GPIO_NUM_25
 
-#define M 3
+#define M 4
+#define N 4
 
-const float bconst = 0.25;
+const float bconst = 0.1;
+const float aconst = 0.9;
 
-static float xbuff[M+1];
-static float b[M+1];
+static float xbuff[M + 1];
+static float b[M + 1] = {0.0127, -0.0311, 0.0420, -0.0311, 0.0127};
 
-void IRAM_ATTR onTime()
-{
-  portENTER_CRITICAL_ISR(&timerMux);
-  count++;
-  portEXIT_CRITICAL_ISR(&timerMux);
+static float ybuff[N];
+static float a[N] = {3.5148, -4.7762, 2.9623, -0.7067};
+
+void IRAM_ATTR onTime() {
+    portENTER_CRITICAL_ISR(&timerMux);
+    count++;
+    portEXIT_CRITICAL_ISR(&timerMux);
 }
 
-void setup()
-{
-  pinMode(inputPin, INPUT_PULLUP);
-  pinMode(outputPin, OUTPUT);
-  pinMode(testPin, OUTPUT);
-  Serial.begin(9600);
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &onTime, true);
+void setup() {
+    pinMode(inputPin, INPUT_PULLUP);
+    pinMode(outputPin, OUTPUT);
+    pinMode(testPin, OUTPUT);
+    Serial.begin(9600);
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &onTime, true);
 
-  // Sets an alarm to sound every second
-  timerAlarmWrite(timer, 1000000 / FREQ, true);
-  timerAlarmEnable(timer);
+    // Sets an alarm to sound every second
+    timerAlarmWrite(timer, 1000000 / FREQ, true);
+    timerAlarmEnable(timer);
 
-  for(int i = 0; i <= M; i++) {
-    xbuff[i] = 0;
-    b[i] = bconst;
-  }
+    for (int i = 0; i <= M; i++) {
+        xbuff[i] = 0;
+    }
+    for (int i = 0; i < N; i++) {
+        ybuff[i] = 0;
+    }
 }
 
-void loop()
-{
-  if (count > 0)
-  {
-    // Comment out enter / exit to deactivate the critical section
-    portENTER_CRITICAL(&timerMux);
-    count--;
-    portEXIT_CRITICAL(&timerMux);
-    float tempinvalue = (float)analogRead(inputPin);
-    
-    digitalWrite(testPin, HIGH);
+void loop() {
+    if (count > 0) {
+        // Comment out enter / exit to deactivate the critical section
+        portENTER_CRITICAL(&timerMux);
+        count--;
+        portEXIT_CRITICAL(&timerMux);
+        float tempinvalue = (float)analogRead(inputPin);
 
-    for(int i = M; i >0; i--) {
-      xbuff[i]= xbuff[i-1];
+        digitalWrite(testPin, HIGH);
+
+        for (int i = M; i > 0; i--) {
+            xbuff[i] = xbuff[i - 1];
+        }
+        xbuff[0] = tempinvalue;
+
+        float outvalue = 0;
+        for (int i = 0; i <= M; i++) {
+            outvalue += b[i] * xbuff[i];
+        }
+
+        for (int i = 0; i < N; i++) {
+            outvalue += a[i] * ybuff[i];
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            ybuff[i] = ybuff[i - 1];
+        }
+        ybuff[0] = outvalue;
+
+        dacWrite(DAC1, (uint32_t)outvalue / 16);
+
+        digitalWrite(testPin, LOW);
     }
-    xbuff[0] = tempinvalue;
-
-    float outvalue = 0;
-    for(int i = 0; i <=M; i++) {
-      outvalue += b[i]*xbuff[i];
-    }
-
-    dacWrite(DAC1, (uint32_t)outvalue / 16);
-
-    digitalWrite(testPin, LOW);
-
-
-  }
 }
